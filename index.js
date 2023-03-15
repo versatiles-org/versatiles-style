531820db0d17b3c4ca0a5a40a2e39dfe2484d39a
yetzt <git@yetzt.me>
2023-03-15 13:37:02 +0100

    add exports for programmatical use

---
+++ index.js
@@ -0,0 +1,6 @@
+// in case someone wants to use this programmatically
+module.exports = exports = {
+	decorate: require("lib/decorate"),
+	layers: require("lib/layers"),
+	template: require("lib/template.json"),
+};
