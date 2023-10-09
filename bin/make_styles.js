d4b26296d28804325760f7dfe4c48de1184a0e9f
Michael Kreil <github@michael-kreil.de>
2023-10-09 13:55:01 +0200

    cleanup

--- bin/make_styles.js
+++ bin/make_styles.js
@@ -9,8 +9,6 @@ import { validateStyleMin } from '@maplibre/maplibre-gl-style-spec';
 const dirRoot = new URL('../', import.meta.url).pathname;
 const dirDst = resolve(dirRoot, 'dist');
 
-// get args --tilejson --glyphs --fonts TODO
-
 // ensure destination dir exists
 if (existsSync(dirDst)) rmSync(dirDst, { recursive: true });
 mkdirSync(dirDst, { recursive: true });
@@ -28,11 +26,13 @@ for (let StyleBakerClass of Object.values(StyleBakerClasses)) {
 
 	function produce(name, options) {
 		const style = styleBaker.bake(options);
+		
 		// Validate the style and log errors if any	
 		let errors = validateStyleMin(style);
 		if (errors.length > 0) console.log(errors);
+
 		// write
 		writeFileSync(resolve(dirDst, name + '.json'), JSON.stringify(style));
 		console.log('Saved ' + name);
 	}
-};
+}
