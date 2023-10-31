a05ab8dddc36146053ff1b7167b80704fb469b54
Michael Kreil <github@michael-kreil.de>
2023-10-31 09:33:03 +0100

    rafactor utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,15 @@
 
 // Utility function to deep clone an object
-export function deepClone(obj) {
+export function deepClone(obj: any): any {
 	return JSON.parse(JSON.stringify(obj));
 }
 
-export function isObject(item) {
+export function isObject(item: any): boolean {
 	return (typeof item === 'object') && !Array.isArray(item);
 }
 
-export function deepMerge(...sources) {
-	let target = {};
+export function deepMerge(...sources: any[]): any {
+	let target: any = {};
 
 	for (let source of sources) {
 		if (!isObject(source)) continue;
@@ -25,11 +25,11 @@ export function deepMerge(...sources) {
 	return target;
 }
 
-export function prettyStyleJSON(data) {
+export function prettyStyleJSON(data: any): string {
 	return recursive(data);
 
-	function recursive(data, prefix = '', path = '') {
-		let type = typeof data;
+	function recursive(data: any, prefix: string = '', path: string = ''): string {
+		let type: string = typeof data;
 		if ((type === 'object') && Array.isArray(data)) type = 'array';
 
 		if (path.endsWith('.bounds')) return singleLine(data);
@@ -48,14 +48,14 @@ export function prettyStyleJSON(data) {
 					'"' + key + '": ' + recursive(value, prefix + '\t', path + '.' + key)
 				).join(',\n\t' + prefix) + '\n' + prefix + '}';
 			case 'array':
-				return '[\n\t' + prefix + data.map(value =>
+				return '[\n\t' + prefix + data.map((value: any) =>
 					recursive(value, prefix + '\t', path + '[]')
 				).join(',\n\t' + prefix) + '\n' + prefix + ']';
 			default: throw new Error('unknown type: ' + type);
 		}
 	}
 
-	function singleLine(data) {
+	function singleLine(data: any): string {
 		return JSON.stringify(data, null, '\t').replace(/[\t\n]+/g, ' ');
 	}
 }
