d0313cffee22ddb0716f6d1a8c01a1b1819583f2
Michael Kreil <github@michael-kreil.de>
2023-11-04 00:19:19 +0100

    improve release script

--- bin/release.ts
+++ bin/release.ts
@@ -1,28 +1,29 @@
 #!/usr/bin/env npx tsx
 
-import { spawn } from 'child_process';
+import { spawn } from 'node:child_process';
+import { readFileSync } from 'node:fs';
+import inquirer from 'inquirer'
 
 process.chdir((new URL('../', import.meta.url)).pathname);
 
-await checkThatNoUncommittedChanges();
+/*
 // check if no git
+await checkThatNoUncommittedChanges();
 
 // lint
 
 // test
 
-// get current version
-// ask for new version
-// set new version in package.json
-// rebuild package.json
-
 // prepare release notes
 
 // build styles
-
+// build node
 // build browser
 
-// npm run build-node
+*/
+// handle version
+const new_version = await getVersion()
+
 // npm release
 
 // git add
@@ -35,9 +36,59 @@ await checkThatNoUncommittedChanges();
 // upload styles
 // upload browser.js
 
+async function getVersion() {
+	// get current version
+	const package_json = JSON.parse(readFileSync('./package.json', 'utf8'));
+	const version_package: string = package_json.version;
+	const version_github: string = await getLatestGitHubTag('versatiles-org/versatiles-styles');
+	if (version_package !== version_github) warn(`versions differ in package.json (${version_package}) and latest GitHub tag (${version_github})`)
+
+	// ask for new version
+	const { version_new } = await inquirer.prompt({
+		message: 'What should be the new version?',
+		name: 'version_new',
+		type: 'list',
+		choices: [bump(2), bump(1), bump(0)]
+	})
+	if (!version_new) throw Error();
+
+	// verify new version
+	const { confirmed } = await inquirer.prompt({
+		message: `Are you sure you want to release the new version ${version_new}?`,
+		name: 'confirmed',
+		type: 'confirm',
+		default: false,
+	})
+	if (!confirmed) abort();
+
+	// set new version in package.json
+
+	// rebuild package.json
+
+	async function getLatestGitHubTag(repo: string): Promise<string> {
+		const res = await fetch(`https://api.github.com/repos/${repo}/tags`);
+		const tags = await res.json() as { name: string }[];
+		const tag = tags.find(tag => tag.name.startsWith('v'))?.name.slice(1);
+		if (!tag) throw Error();
+		return tag;
+	}
+	function bump(index: 0 | 1 | 2) {
+		const p = version_package.split('.').map(v => parseInt(v, 10));
+		if (p.length !== 3) throw Error();
+		switch (index) {
+			case 0: p[0]++; p[1] = 0; p[2] = 0; break;
+			case 1: p[1]++; p[2] = 0; break;
+			case 2: p[2]++; break;
+		}
+		const name = p.map((n, i) => (i == index) ? `\x1b[1m${n}` : `${n}`).join('.') + '\x1b[22m';
+		const value = p.join('.');
+		return { name, value }
+	}
+}
 
 async function checkThatNoUncommittedChanges() {
-	console.log(await run('git status --porcelain'));
+	if ((await run('git status --porcelain')).stdout.length < 3) return;
+	panic('please commit all changes before releasing');
 }
 
 function run(command: string): Promise<{ code: number | null, signal: string | null, stdout: string, stderr: string }> {
@@ -53,3 +104,7 @@ function run(command: string): Promise<{ code: number | null, signal: string | n
 		cp.stderr?.on('data', chunk => stderr.push(chunk));
 	})
 }
+
+function panic(text: string) { process.stderr.write(`\x1b[1;31m! ERROR: ${text}\x1b[0m\n`); abort(); }
+function warn(text: string) { process.stderr.write(`'\x1b[1;33m! warning: ${text}\x1b[0m\n`); }
+function abort() { process.stderr.write('\x1b[1;31m! ABORT\x1b[0m\n'); process.exit(); }
