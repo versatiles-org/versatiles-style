dd285350ba88c291955d0790687f1b5b2d126df7
Michael Kreil <github@michael-kreil.de>
2023-11-04 20:27:54 +0100

    fix index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,19 +2,25 @@ import * as styleBuilders from './index.js';
 import { StyleBuilder } from './lib/style_builder.js';
 
 describe('Style Builders', () => {
-	let styleNames = ['Colorful', 'Graybeard', 'Neutrino'];
+	let styleNames = ['colorful', 'graybeard', 'neutrino'];
 
 	for (const styleName of styleNames) {
 		it(`should create and test an instance of ${styleName}`, () => {
 			// @ts-ignore
 			const styleBuilder = styleBuilders[styleName] as StyleBuilder;
-			expect(styleBuilder).toBeDefined();
 			expect(styleBuilder).toBeInstanceOf(StyleBuilder);
 			expect(typeof styleBuilder.name).toBe('string');
 			expect(typeof styleBuilder.defaultOptions).toBe('object');
-			const style = styleBuilder.build();
-			expect(typeof style).toBe('object');
+
+			const style = styleBuilder.build({ baseUrl: 'https://example.org' });
 			expect(JSON.stringify(style).length).toBeGreaterThan(50000);
+
+			expect(style.name).toBe('versatiles-' + styleName);
+			expect(style.glyphs).toBe('https://example.org/assets/fonts/{fontstack}/{range}.pbf');
+			expect(style.sprite).toBe('https://example.org/assets/sprites/sprites');
+			expect(Object.keys(style.sources).join(',')).toBe('versatiles-shortbread');
+			// @ts-ignore
+			expect(style.sources['versatiles-shortbread'].tiles).toEqual(['https://example.org/tiles/osm/{z}/{x}/{y}']);
 		});
 	}
 });
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import ColorfulClass from './styles/colorful.js';
 import GraybeardClass from './styles/graybeard.js';
 import NeutrinoClass from './styles/neutrino.js';
 
-export const Colorful = (new ColorfulClass()).getBuilder();
-export const Graybeard = (new GraybeardClass()).getBuilder();
-export const Neutrino = (new NeutrinoClass()).getBuilder();
+export const colorful = (new ColorfulClass()).getBuilder();
+export const graybeard = (new GraybeardClass()).getBuilder();
+export const neutrino = (new NeutrinoClass()).getBuilder();
