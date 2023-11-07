cc1b8081c89d85ae6ce480315b0e82250c22075b
Michael Kreil <github@michael-kreil.de>
2023-11-07 18:11:20 +0100

    fix: error handling

--- scripts/lib/shell.ts
+++ scripts/lib/shell.ts
@@ -1,23 +1,34 @@
 import { spawn } from 'child_process';
 
 export async function run(command: string, errorOnCodeZero?: boolean): Promise<{ code: number | null; signal: string | null; stdout: string; stderr: string }> {
-	return new Promise((res, rej) => {
-		const stdout: Buffer[] = [];
-		const stderr: Buffer[] = [];
-		const cp = spawn('bash', ['-c', command])
-			.on('error', error => {
-				rej(error);
-			})
-			.on('close', (code, signal) => {
-				const result = { code, signal, stdout: Buffer.concat(stdout).toString(), stderr: Buffer.concat(stderr).toString() };
-				if ((errorOnCodeZero ?? true) && (code !== 0)) {
-					rej(result); return;
-				}
-				res(result);
-			});
-		cp.stdout.on('data', (chunk: Buffer) => stdout.push(chunk));
-		cp.stderr.on('data', (chunk: Buffer) => stderr.push(chunk));
-	});
+	try {
+		return await new Promise((resolve, reject) => {
+			const stdout: Buffer[] = [];
+			const stderr: Buffer[] = [];
+			const cp = spawn('bash', ['-c', command])
+				.on('error', error => {
+					reject(error);
+				})
+				.on('close', (code, signal) => {
+					const result = {
+						code,
+						signal,
+						stdout: Buffer.concat(stdout).toString(),
+						stderr: Buffer.concat(stderr).toString(),
+					};
+					if ((errorOnCodeZero ?? true) && (code !== 0)) {
+						reject(result);
+					} else {
+						resolve(result);
+					}
+				});
+			cp.stdout.on('data', (chunk: Buffer) => stdout.push(chunk));
+			cp.stderr.on('data', (chunk: Buffer) => stderr.push(chunk));
+		});
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
 }
 
 run.stderr = async (command: string, errorOnCodeZero?: boolean): Promise<string> =>
