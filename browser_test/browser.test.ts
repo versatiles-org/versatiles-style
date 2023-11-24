e22f45586bc74821639530fdea2737990cb5b41f
Michael Kreil <github@michael-kreil.de>
2023-11-24 01:21:43 +0100

    test: language

--- browser_test/browser.test.ts
+++ browser_test/browser.test.ts
@@ -1,5 +1,7 @@
 
+import type { SymbolLayerSpecification } from '@maplibre/maplibre-gl-style-spec';
 import * as V from '../release/versatiles-style.js';
+import type { LanguageSuffix } from '../src/lib/types.js';
 
 type Builder = typeof V.Colorful | typeof V.Neutrino;
 type Style = V.Colorful | V.Neutrino;
@@ -7,18 +9,16 @@ type Style = V.Colorful | V.Neutrino;
 interface StyleTestConfig {
 	name: string;
 	builder: Builder;
+	labelLayers?: RegExp;
 }
 
-const styles: StyleTestConfig[] = [
-	{ name: 'Colorful', builder: V.Colorful },
-	{ name: 'Graybeard', builder: V.Graybeard },
+[
+	{ name: 'Colorful', builder: V.Colorful, labelLayers: /^label-(street|place|boundary|transit)-/ },
+	{ name: 'Graybeard', builder: V.Graybeard, labelLayers: /^label-(street|place|boundary|transit)-/ },
 	{ name: 'Neutrino', builder: V.Neutrino },
-];
-
-for (const style of styles) {
-	test(style);
-}
-
+].forEach(config => {
+	test(config);
+});
 
 function test(config: StyleTestConfig): void {
 
@@ -29,6 +29,19 @@ function test(config: StyleTestConfig): void {
 		it('should be buildable', () => {
 			expect(style.build()).toBeTruthy();
 		});
-	});
 
+		if (config.labelLayers) {
+			it('should use correct language suffix', () => {
+				(['', '_de', '_en'] as LanguageSuffix[]).forEach(langSuffix => {
+					style.languageSuffix = langSuffix;
+					const textLayers = style.build().layers
+						.filter(l => config.labelLayers?.test(l.id))
+						.filter(l => l.type === 'symbol') as SymbolLayerSpecification[];
+					const layersTextFields = textLayers.map(l => l.layout?.['text-field'] as unknown);
+					expect(layersTextFields)
+						.toMatchObject(layersTextFields.slice().fill(`{name${langSuffix}}`));
+				});
+			});
+		}
+	});
 }
