4508ebcfc22a0150ca192b408bb20f95d4b64a8e
Michael Kreil <github@michael-kreil.de>
2023-11-29 14:57:27 +0100

    doc: types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -11,12 +11,19 @@ import type {
 import type Color from 'color';
 import type StyleBuilder from './style_builder.ts';
 
+/** Represents the available tile formats. */
 export type TileFormat = 'avif' | 'bin' | 'geojson' | 'jpg' | 'json' | 'pbf' | 'png' | 'svg' | 'topojson' | 'webp';
 
+/** Type for Maplibre layers, including background, fill, line, and symbol specifications. */
 export type MaplibreLayer = BackgroundLayerSpecification | FillLayerSpecification | LineLayerSpecification | SymbolLayerSpecification;
+
+/** Defines the structure of Maplibre layer definitions, omitting the 'source' property for fill, line, and symbol specifications. */
 export type MaplibreLayerDefinition = BackgroundLayerSpecification | Omit<FillLayerSpecification, 'source'> | Omit<LineLayerSpecification, 'source'> | Omit<SymbolLayerSpecification, 'source'>;
+
+/** Represents a filter specification in Maplibre styles. */
 export type MaplibreFilter = FilterSpecification;
 
+/** Represents the structure of a vector layer in a TileJSON specification. */
 export interface VectorLayer {
 	id: string;
 	fields: Record<string, 'Boolean' | 'Number' | 'String'>;
@@ -25,6 +32,7 @@ export interface VectorLayer {
 	maxzoom?: number;
 }
 
+/** Basic structure for TileJSON specification, applicable to both raster and vector types. */
 export interface TileJSONSpecificationBasic {
 	tilejson?: '3.0.0';
 	attribution?: string;
@@ -42,51 +50,76 @@ export interface TileJSONSpecificationBasic {
 	template?: string;
 }
 
+/** Structure for TileJSON specification of raster type, specifying raster-specific properties. */
 export interface TileJSONSpecificationRaster extends TileJSONSpecificationBasic {
 	type: 'raster';
 	format: 'avif' | 'jpg' | 'png' | 'webp';
 }
+
+/** Structure for TileJSON specification of vector type, specifying vector-specific properties. */
 export interface TileJSONSpecificationVector extends TileJSONSpecificationBasic {
 	type: 'vector';
 	format: 'pbf';
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	vector_layers: VectorLayer[];
 }
+
+/** Represents a TileJSON specification, which can be either raster or vector. */
 export type TileJSONSpecification = TileJSONSpecificationRaster | TileJSONSpecificationVector;
 
+/** Options for creating TileJSON, extending the basic specification with format and optional vector layers. */
 export interface TileJSONOption extends TileJSONSpecificationBasic {
 	format: 'avif' | 'jpg' | 'pbf' | 'png' | 'webp';
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	vector_layers?: VectorLayer[];
 }
 
+/** Type for Maplibre styles specifically designed for raster sources. */
 export type MaplibreStyleRaster = Omit<StyleSpecification, 'sources'> & {
 	// eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 	'sources': { [_: string]: TileJSONSpecificationRaster };
 };
+
+/** Type for Maplibre styles specifically designed for vector sources. */
 export type MaplibreStyleVector = Omit<StyleSpecification, 'sources'> & {
 	// eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 	'sources': { [_: string]: TileJSONSpecificationVector };
 };
+
+/** Represents a Maplibre style, which can be either raster or vector. */
 export type MaplibreStyle = MaplibreStyleRaster | MaplibreStyleVector;
 
-
+/** Defines the value type for a style rule. */
 export type StyleRuleValue = boolean | number | object | string;
+
+/** Defines the structure of a style rule, which is a record of properties to style values. */
 export type StyleRule = Record<string, StyleRuleValue | undefined>;
+
+/** Defines the structure of style rules, which is a record of selectors to style rules. */
 export type StyleRules = Record<string, StyleRule | undefined>;
-//export type StylemakerColorLookup = Record<string, Color | undefined>;
-//export type StylemakerStringLookup = Record<string, string | undefined>;
+
+/** Represents language suffixes used in map styles. */
 export type LanguageSuffix = '_de' | '_en' | '';
 
+/** Defines the keys for color properties in a style builder. */
 export type StylemakerColorKeys<T extends StyleBuilder<T>> = keyof T['defaultColors'];
+
+/** Defines the keys for font properties in a style builder. */
 export type StylemakerFontKeys<T extends StyleBuilder<T>> = keyof T['defaultFonts'];
 
+/** Records string values for color properties in a style builder. */
 export type StylemakerColorStrings<T extends StyleBuilder<T>> = Record<StylemakerColorKeys<T>, string>;
+
+/** Records string values for font properties in a style builder. */
 export type StylemakerFontStrings<T extends StyleBuilder<T>> = Record<StylemakerFontKeys<T>, string>;
 
+/** Records Color objects for color properties in a style builder. */
 export type StylemakerColors<T extends StyleBuilder<T>> = Record<StylemakerColorKeys<T>, Color>;
+
+/** Records string values for font properties in a style builder. */
 export type StylemakerFonts<T extends StyleBuilder<T>> = Record<StylemakerFontKeys<T>, string>;
 
+/** Defines options for style rules in a style builder. */
 export interface StyleRulesOptions<T extends StyleBuilder<T>> {
 	colors: StylemakerColors<T>;
 	fonts: StylemakerFontStrings<T>;
@@ -94,30 +127,66 @@ export interface StyleRulesOptions<T extends StyleBuilder<T>> {
 }
 
 export interface RecolorOptions {
+
+	/** If true, inverts the colors. */
 	invert?: boolean;
+
+	/** The degree to rotate the hue of the color (in degrees). */
 	rotate?: number;
+
+	/** Adjusts the saturation level of the color. Positive values increase saturation, negative values decrease it. */
 	saturate?: number;
+
+	/** Adjusts the gamma of the color. Affects the brightness in a non-linear manner. */
 	gamma?: number;
+
+	/** Adjusts the contrast of the color. Higher values produce more contrast. */
 	contrast?: number;
+
+	/** Adjusts the brightness of the color. Positive values make it brighter, negative values make it darker. */
 	brightness?: number;
+
+	/** Specifies the intensity of the tinting effect. Ranges from 0 (no effect) to 1 (full effect). */
 	tint?: number;
+
+	/** Specifies the color used for tinting, in a string format (e.g., '#FF0000'). */
 	tintColor?: string;
 }
 
 export interface StylemakerOptions<T extends StyleBuilder<T>> {
+
+	/** The base URL for loading external resources like tiles, sprites, and fonts. */
 	baseUrl?: string;
+
+	/** The URL template for loading font glyphs, formatted with `{fontstack}` and `{range}` placeholders. */
 	glyphsUrl?: string;
+
+	/** The URL for loading sprite images and metadata. */
 	spriteUrl?: string;
+
+	/** An array of URL templates for loading map tiles, with `{z}`, `{x}`, and `{y}` placeholders. */
 	tilesUrls?: string[];
+
+	/** If true, hides all map labels. */
 	hideLabels?: boolean;
+
+	/** Suffix to append to language-specific resources, such as `"-en"` for English. */
 	languageSuffix?: LanguageSuffix;
+
+	/** An object specifying overrides for default color values, keyed by the color names. */
 	colors?: Partial<StylemakerColorStrings<T>>;
+
+	/** An object specifying overrides for default font names, keyed by the font names. */
 	fonts?: Partial<StylemakerFontStrings<T>>;
+
+	/** Options for color adjustments and transformations applied to the entire style. */
 	recolor?: RecolorOptions;
 }
 
-
-
+/** 
+* Checks if an object adheres to the TileJSON specification. 
+* Throws errors if the object does not conform to the expected structure or types.
+*/
 export function isTileJSONSpecification(obj: unknown): obj is TileJSONSpecification {
 	if (typeof obj !== 'object' || obj === null) {
 		throw Error('spec must be an object');
@@ -188,7 +257,11 @@ export function isTileJSONSpecification(obj: unknown): obj is TileJSONSpecificat
 	return true;
 }
 
-function validateVectorLayer(obj: unknown): boolean {
+/** 
+* Validates if an object conforms to the VectorLayer structure. 
+* Throws errors for any deviations from the expected structure or types.
+*/
+export function validateVectorLayer(obj: unknown): boolean {
 	if (typeof obj !== 'object' || obj === null) {
 		throw Error('layer must be an object');
 	}
