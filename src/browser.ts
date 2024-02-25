90ced047bd608f9c56a3ec89c6bf232ad2216a8c
Michael Kreil <github@michael-kreil.de>
2024-02-25 21:08:14 +0100

    fix: styles are under styles in browser

--- src/browser.ts
+++ src/browser.ts
@@ -1,6 +1,6 @@
 
 export type * from './styles';
-export * from './styles';
+export * as styles from './styles';
 
 export type { GuessStyleOptions } from './guess_style';
 export { guessStyle } from './guess_style';
