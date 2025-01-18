9ecdfe4b5f97b6d7152bfc1a3f821d71afc7ce0c
Michael Kreil <github@michael-kreil.de>
2025-01-18 15:47:35 +0100

    test: improve Color.RGB

--- src/color/rgb.test.ts
+++ src/color/rgb.test.ts
@@ -41,29 +41,71 @@ describe('RGB Class', () => {
 		expect(color2.asHex()).toBe('#FF804080');
 	});
 
-	test('asHSL converts RGB to HSL correctly', () => {
-		const color = new RGB(255, 0, 0);
-		const hsl = color.asHSL();
-		expect(hsl).toBeInstanceOf(HSL);
-		expect(hsl.asArray().map(value => Math.round(value)))
-			.toStrictEqual([0, 100, 50, 1]);
+	describe('color conversion', () => {
+		test('asHSL converts RGB to HSL correctly', () => {
+			const hsl = new RGB(255, 0, 0).asHSL();
+			expect(hsl).toBeInstanceOf(HSL);
+			expect(hsl.asArray().map(value => Math.round(value)))
+				.toStrictEqual([0, 100, 50, 1]);
+
+			expect(RGB.parse('#000000').asHSL().round().asArray()).toStrictEqual([0, 0, 0, 1]);
+			expect(RGB.parse('#FFFFFF').asHSL().round().asArray()).toStrictEqual([0, 0, 100, 1]);
+			expect(RGB.parse('#FF0000').asHSL().round().asArray()).toStrictEqual([0, 100, 50, 1]);
+			expect(RGB.parse('#FF8000').asHSL().round().asArray()).toStrictEqual([30, 100, 50, 1]);
+			expect(RGB.parse('#FFFF00').asHSL().round().asArray()).toStrictEqual([60, 100, 50, 1]);
+			expect(RGB.parse('#80FF00').asHSL().round().asArray()).toStrictEqual([90, 100, 50, 1]);
+			expect(RGB.parse('#00FF00').asHSL().round().asArray()).toStrictEqual([120, 100, 50, 1]);
+			expect(RGB.parse('#00FF80').asHSL().round().asArray()).toStrictEqual([150, 100, 50, 1]);
+			expect(RGB.parse('#00FFFF').asHSL().round().asArray()).toStrictEqual([180, 100, 50, 1]);
+			expect(RGB.parse('#0080FF').asHSL().round().asArray()).toStrictEqual([210, 100, 50, 1]);
+			expect(RGB.parse('#0000FF').asHSL().round().asArray()).toStrictEqual([240, 100, 50, 1]);
+			expect(RGB.parse('#8000FF').asHSL().round().asArray()).toStrictEqual([270, 100, 50, 1]);
+			expect(RGB.parse('#FF00FF').asHSL().round().asArray()).toStrictEqual([300, 100, 50, 1]);
+			expect(RGB.parse('#FF0080').asHSL().round().asArray()).toStrictEqual([330, 100, 50, 1]);
+		});
+
+		test('asHSV converts RGB to HSV correctly', () => {
+			const hsv = new RGB(255, 0, 0).asHSV();
+			expect(hsv).toBeInstanceOf(HSV);
+			expect(hsv.asArray().map(value => Math.round(value)))
+				.toStrictEqual([0, 100, 100, 1]);
+
+			expect(RGB.parse('#000000').asHSV().round().asArray()).toStrictEqual([0, 0, 0, 1]);
+			expect(RGB.parse('#FFFFFF').asHSV().round().asArray()).toStrictEqual([0, 0, 100, 1]);
+			expect(RGB.parse('#FF0000').asHSV().round().asArray()).toStrictEqual([0, 100, 100, 1]);
+			expect(RGB.parse('#FF8000').asHSV().round().asArray()).toStrictEqual([30, 100, 100, 1]);
+			expect(RGB.parse('#FFFF00').asHSV().round().asArray()).toStrictEqual([60, 100, 100, 1]);
+			expect(RGB.parse('#80FF00').asHSV().round().asArray()).toStrictEqual([90, 100, 100, 1]);
+			expect(RGB.parse('#00FF00').asHSV().round().asArray()).toStrictEqual([120, 100, 100, 1]);
+			expect(RGB.parse('#00FF80').asHSV().round().asArray()).toStrictEqual([150, 100, 100, 1]);
+			expect(RGB.parse('#00FFFF').asHSV().round().asArray()).toStrictEqual([180, 100, 100, 1]);
+			expect(RGB.parse('#0080FF').asHSV().round().asArray()).toStrictEqual([210, 100, 100, 1]);
+			expect(RGB.parse('#0000FF').asHSV().round().asArray()).toStrictEqual([240, 100, 100, 1]);
+			expect(RGB.parse('#8000FF').asHSV().round().asArray()).toStrictEqual([270, 100, 100, 1]);
+			expect(RGB.parse('#FF00FF').asHSV().round().asArray()).toStrictEqual([300, 100, 100, 1]);
+			expect(RGB.parse('#FF0080').asHSV().round().asArray()).toStrictEqual([330, 100, 100, 1]);
+		});
+
+		test('asRGB and toRGB return the same instance or clone', () => {
+			const color = new RGB(255, 128, 64, 0.5);
+			expect(color.asRGB()).toStrictEqual(color);
+			expect(color.toRGB()).toStrictEqual(color);
+		});
+
+		test('handles black correctly', () => {
+			const color = new RGB(0, 0, 0);
+			expect(color.asHSL().asArray().map(value => Math.round(value))).toStrictEqual([0, 0, 0, 1]);
+			expect(color.asHSV().asArray().map(value => Math.round(value))).toStrictEqual([0, 0, 0, 1]);
+		});
+
+		test('handles white correctly', () => {
+			const color = new RGB(255, 255, 255);
+			expect(color.asHSL().asArray().map(value => Math.round(value))).toStrictEqual([0, 0, 100, 1]);
+			expect(color.asHSV().asArray().map(value => Math.round(value))).toStrictEqual([0, 0, 100, 1]);
+		});
 	});
 
-	test('asHSV converts RGB to HSV correctly', () => {
-		const color = new RGB(255, 0, 0);
-		const hsv = color.asHSV();
-		expect(hsv).toBeInstanceOf(HSV);
-		expect(hsv.asArray().map(value => Math.round(value)))
-			.toStrictEqual([0, 100, 100, 1]);
-	});
-
-	test('asRGB and toRGB return the same instance or clone', () => {
-		const color = new RGB(255, 128, 64, 0.5);
-		expect(color.asRGB()).toStrictEqual(color);
-		expect(color.toRGB()).toStrictEqual(color);
-	});
-
-	describe('parse method', () => {
+	describe('parse', () => {
 		test('parses hexadecimal color strings correctly', () => {
 			expect(RGB.parse('#ff8040').asArray()).toStrictEqual([255, 128, 64, 1]);
 			expect(RGB.parse('#ff804066').asArray()).toStrictEqual([255, 128, 64, 0.4]);
@@ -87,17 +129,100 @@ describe('RGB Class', () => {
 		});
 	});
 
-	describe('edge cases for conversions', () => {
-		test('handles black correctly', () => {
-			const color = new RGB(0, 0, 0);
-			expect(color.asHSL().asArray().map(value => Math.round(value))).toStrictEqual([0, 0, 0, 1]);
-			expect(color.asHSV().asArray().map(value => Math.round(value))).toStrictEqual([0, 0, 0, 1]);
+	describe('filter methods', () => {
+		function pc(cb: (c: RGB) => RGB): [number, number, number, number] {
+			return cb(new RGB(50, 150, 200, 0.8)).round().asArray();
+		}
+
+		describe('gamma', () => {
+			test('adjusts gamma correctly', () => {
+				expect(pc(c => c.gamma(2.2))).toStrictEqual([7, 79, 149, 0.8]);
+			});
+
+			test('clamps extreme gamma values', () => {
+				expect(pc(c => c.gamma(0.001))).toStrictEqual([255, 255, 255, 0.8]);
+				expect(pc(c => c.gamma(1000))).toStrictEqual([0, 0, 0, 0.8]);
+			});
 		});
 
-		test('handles white correctly', () => {
-			const color = new RGB(255, 255, 255);
-			expect(color.asHSL().asArray().map(value => Math.round(value))).toStrictEqual([0, 0, 100, 1]);
-			expect(color.asHSV().asArray().map(value => Math.round(value))).toStrictEqual([0, 0, 100, 1]);
+		describe('invert', () => {
+			test('inverts RGB values correctly', () => {
+				expect(pc(c => c.invert())).toStrictEqual([205, 105, 55, 0.8]);
+			});
+		});
+
+		describe('contrast', () => {
+			test('adjusts contrast correctly', () => {
+				expect(pc(c => c.contrast(1.5))).toStrictEqual([11, 161, 236, 0.8]);
+			});
+
+			test('clamps extreme contrast values', () => {
+				expect(pc(c => c.contrast(1e6))).toStrictEqual([0, 255, 255, 0.8]);
+				expect(pc(c => c.contrast(0))).toStrictEqual([128, 128, 128, 0.8]);
+			});
+		});
+
+		describe('brightness', () => {
+			test('increases brightness correctly', () => {
+				expect(pc(c => c.brightness(0.5))).toStrictEqual([153, 203, 228, 0.8]);
+			});
+
+			test('decreases brightness correctly', () => {
+				expect(pc(c => c.brightness(-0.5))).toStrictEqual([25, 75, 100, 0.8]);
+			});
+
+			test('clamps brightness values', () => {
+				expect(pc(c => c.brightness(2))).toStrictEqual([255, 255, 255, 0.8]);
+				expect(pc(c => c.brightness(-2))).toStrictEqual([0, 0, 0, 0.8]);
+			});
+		});
+
+		describe('tint', () => {
+			test('tints color correctly', () => {
+				const tintColor = new RGB(255, 0, 0);
+				expect(pc(c => c.tint(0.5, tintColor))).toStrictEqual([125, 100, 125, 0.8]);
+			});
+
+			test('handles extreme tint values', () => {
+				const tintColor = new RGB(255, 0, 0);
+				expect(pc(c => c.tint(1, tintColor))).toStrictEqual([200, 50, 50, 0.8]);
+				expect(pc(c => c.tint(0, tintColor))).toStrictEqual([50, 150, 200, 0.8]);
+			});
+		});
+
+		describe('lighten', () => {
+			test('lightens the color correctly', () => {
+				expect(pc(c => c.lighten(0.5))).toStrictEqual([153, 203, 228, 0.8]);
+			});
+
+			test('clamps lighten ratio', () => {
+				expect(pc(c => c.lighten(2))).toStrictEqual([255, 255, 255, 0.8]);
+			});
+		});
+
+		describe('darken', () => {
+			test('darkens the color correctly', () => {
+				expect(pc(c => c.darken(0.5))).toStrictEqual([25, 75, 100, 0.8]);
+			});
+
+			test('clamps darken ratio', () => {
+				expect(pc(c => c.darken(1))).toStrictEqual([0, 0, 0, 0.8]);
+				expect(pc(c => c.darken(2))).toStrictEqual([0, 0, 0, 0.8]);
+			});
+		});
+
+		describe('fade', () => {
+			test('reduces alpha correctly', () => {
+				expect(pc(c => c.fade(0.5))).toStrictEqual([50, 150, 200, 0.4]);
+			});
+
+			test('handles extreme fade values', () => {
+				expect(pc(c => c.fade(1))).toStrictEqual([50, 150, 200, 0]);
+
+				const fullyOpaque = new RGB(50, 150, 200, 1);
+				expect(fullyOpaque.fade(0).asArray()).toStrictEqual([50, 150, 200, 1]);
+			});
 		});
 	});
 });
+
