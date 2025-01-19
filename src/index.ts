8a6bd246b092ea09c90ffb8d6f9dc4d42edd7972
Michael Kreil <github@michael-kreil.de>
2025-01-19 14:55:16 +0100

    fix: export Color types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export type * from './styles/index.js';
 export * as styles from './styles/index.js';
 
 export type { GuessStyleOptions } from './guess_style/index.js';
+export type { RGB, HSL, HSV, RandomColorOptions } from './color/index.js';
 export type { TileJSONSpecification, TileJSONSpecificationRaster, TileJSONSpecificationVector } from './types/tilejson.js';
 export type { VectorLayer } from './types/index.js';
 export type { StyleBuilderOptions, StyleBuilderColorStrings, StyleBuilderFontStrings, Language, StyleBuilderColorKeys, StyleBuilderFontKeys } from './style_builder/types.js';
