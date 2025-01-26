e54b47d0eeb9edd20d906481a5451955b9a868f1
Michael Kreil <github@michael-kreil.de>
2025-01-26 15:40:45 +0100

    build: overhauled typescript, rollup and package

---
+++ rollup.config.ts
@@ -0,0 +1,51 @@
+import commonjs from '@rollup/plugin-commonjs';
+import sourcemaps from 'rollup-plugin-sourcemaps2';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import process from 'node:process';
+import type { RollupOptions } from 'rollup';
+
+const { BUILD } = process.env;
+const browser = BUILD === 'browser';
+
+const config: RollupOptions[] = [
+	{
+		input: 'src/index.ts',
+		output: {
+			file: browser ? 'release/versatiles-style/versatiles-style.js' : 'dist/index.js',
+			format: 'umd',
+			sourcemap: true,
+			indent: !browser,
+			name: 'VersaTilesStyle'
+		},
+		plugins: [
+			browser && terser({ compress: { pure_getters: true, passes: 3 }, sourceMap: true }),
+			nodeResolve({ browser }),
+			typescript({
+				tsconfig: './tsconfig.json',
+				include: ['src/**/*.ts'],
+				exclude: ['**/*.test.ts'],
+				sourceMap: true,
+				declaration: true,
+				declarationDir: browser ? 'release/versatiles-style/declaration' : 'dist/declaration',
+			}),
+			commonjs(),
+			sourcemaps(),
+		],
+		onLog(level, log, handler) {
+			if (log.code === 'CIRCULAR_DEPENDENCY') return;
+			handler(level, log);
+		}
+	}, {
+		input: browser ? 'release/versatiles-style/declaration/index.d.ts' : 'dist/declaration/index.d.ts',
+		output: {
+			file: browser ? 'release/versatiles-style/versatiles-style.d.ts' : 'dist/index.d.ts',
+			format: 'es'
+		},
+		plugins: [dts()],
+	}
+]
+
+export default config;
