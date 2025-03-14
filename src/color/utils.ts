5fb8c389d476683873a839e96c2556dde6b473c8
Michael Kreil <github@michael-kreil.de>
2025-03-14 23:04:33 +0100

    refactor: improve clamp and mod

--- src/color/utils.ts
+++ src/color/utils.ts
@@ -1,11 +1,17 @@
 
 
-export function clamp(num: number, min: number, max: number): number {
-	return Math.min(Math.max(min, num), max);
+export function clamp(value: number, min: number, max: number): number {
+	if (value == null || isNaN(value)) return min;
+	if (value < min) return min;
+	if (value > max) return max;
+	return value;
 }
 
-export function mod(num: number, max: number): number {
-	return ((num % max) + max) % max;
+export function mod(value: number, max: number): number {
+	value = value % max;
+	if (value < 0) value += max;
+	if (value == 0) return 0;
+	return value;
 }
 
 export function formatFloat(num: number, precision: number): string {
