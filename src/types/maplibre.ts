ec2c994b60f18a25fe6d7308be831dc5c03dfccb
yetzt <git@yetzt.me>
2025-04-20 01:34:06 +0200

    allow source property in layers, allow raster layers

--- src/types/maplibre.ts
+++ src/types/maplibre.ts
@@ -1,11 +1,11 @@
-import type { BackgroundLayerSpecification, FillLayerSpecification, FilterSpecification, LineLayerSpecification, SymbolLayerSpecification } from '@maplibre/maplibre-gl-style-spec';
+import type { BackgroundLayerSpecification, FillLayerSpecification, FilterSpecification, LineLayerSpecification, SymbolLayerSpecification, RasterLayerSpecification } from '@maplibre/maplibre-gl-style-spec';
 export type { StyleSpecification } from '@maplibre/maplibre-gl-style-spec';
 
 /** Type for Maplibre layers, including background, fill, line, and symbol specifications. */
 export type MaplibreLayer = BackgroundLayerSpecification | FillLayerSpecification | LineLayerSpecification | SymbolLayerSpecification;
 
-/** Defines the structure of Maplibre layer definitions, omitting the 'source' property for fill, line, and symbol specifications. */
-export type MaplibreLayerDefinition = BackgroundLayerSpecification | Omit<FillLayerSpecification, 'source'> | Omit<LineLayerSpecification, 'source'> | Omit<SymbolLayerSpecification, 'source'>;
+/** Defines the structure of Maplibre layer definitions. */
+export type MaplibreLayerDefinition = BackgroundLayerSpecification | FillLayerSpecification | LineLayerSpecification | SymbolLayerSpecification | RasterLayerSpecification;
 
 /** Represents a filter specification in Maplibre styles. */
 export type MaplibreFilter = FilterSpecification;
