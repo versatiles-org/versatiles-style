3ddc987b77f4bfdd60a95ceb3e09440a5698e161
Michael Kreil <github@michael-kreil.de>
2026-07-03 09:59:16 +0200

    feat: update layer handling in OsmOverlay and Osm options to use resolved layer groups

--- src/options/osm-overlay.ts
+++ src/options/osm-overlay.ts
@@ -8,15 +8,15 @@ import type { TextOptions, ResolvedText } from './text.js';
 import { resolveText } from './text.js';
 import type { LayoutOptions, ResolvedLayout } from './layout.js';
 import { resolveLayout } from './layout.js';
-import type { LayerGroupOptions } from './layer-groups.js';
+import { resolveLayerGroups, type LayerGroupOptions, type ResolvedLayerGroupOptions } from './layer-groups.js';
 
 export type OsmOverlayOptions = {
 	theme?:
-		| Palette
-		| {
-				darkMode?: boolean | 'auto';
-				palette?: Palette;
-		  };
+	| Palette
+	| {
+		darkMode?: boolean | 'auto';
+		palette?: Palette;
+	};
 	layers?: LayerGroupOptions;
 	text?: TextOptions;
 	layout?: LayoutOptions;
@@ -26,7 +26,7 @@ export type OsmOverlayOptions = {
 
 export type ResolvedOsmOverlayOptions = {
 	theme: ResolvedTheme;
-	layers: LayerGroupOptions;
+	layers: ResolvedLayerGroupOptions;
 	text: ResolvedText;
 	layout: ResolvedLayout;
 	colors: ResolvedColors;
@@ -37,7 +37,7 @@ export function resolveOsmOverlayOptions(content: OsmOverlayOptions): ResolvedOs
 	const theme = resolveTheme(content.theme);
 	return {
 		theme,
-		layers: content.layers ?? {},
+		layers: resolveLayerGroups(content.layers),
 		text: resolveText(content.text),
 		layout: resolveLayout(content.layout),
 		colors: resolveColors(theme, content.colors),
