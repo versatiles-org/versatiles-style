4eca1bd4cd17a1ae17650f46d448e3a607196c06
Michael Kreil <github@michael-kreil.de>
2026-07-03 12:14:26 +0200

    refactor: update opacity handling for POI icons to support varying zoom levels

--- src/shortbread/layers/pois.ts
+++ src/shortbread/layers/pois.ts
@@ -260,7 +260,7 @@ export function* pois(ctx: LayerContext): Generator<b.TaggedLayer> {
 			filter: ['to-boolean', ['get', key]],
 			minzoom: 14, // Shortbread `pois` appear at z14 (matches OSM Bright poi layers)
 			iconSize: { 16: 0.5, 19: 0.5, 20: 1 },
-			opacity: iconOpacity,
+			opacity: { 16: 0, 17: iconOpacity },
 			symbolPlacement: 'point',
 			iconOptional: true,
 			font: ctx.fonts.normal,
